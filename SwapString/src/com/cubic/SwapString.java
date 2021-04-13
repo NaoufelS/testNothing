635e839f42df7d9f5ee68c4142216087ea8095f8
LENOVO USER <seghirinaoufel@gmail.com>
2021-04-12 21:38:59 -0500

    1st comits Upload all files test

---
+++ SwapString/src/com/cubic/SwapString.java
@@ -0,0 +1,19 @@
+package com.cubic;
+
+public class SwapString {
+	
+	public static String a = "Java";
+	public static String b = "Udemy";
+
+	public static void main(String[] args) {
+		String c;
+		c=a;
+		a=b;
+		b=c;
+		System.out.println("a is:"+ a);
+		System.out.println("b is:"+b);
+		
+
+	}
+
+}
